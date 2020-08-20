0fe780d56c4b813287f1183819e1f38c597837af
lipeizhao <lipeizhao@encootech.com>
2020-08-20 11:03:35 +0800

    add class instance varaible

--- class/class_instance.py
+++ class/class_instance.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 class A:
     abc = 123
     def __init__(self):
@@ -5,7 +6,6 @@ class A:
         print(self.abc)
         self.abc = 0
         print("exit init ...")
-
 
 a = A()
 print(a.abc)
@@ -18,3 +18,42 @@ class B:
 
 b = B()
 print(b.__dict__)
+
+class C:
+    attr: int = 1
+    percent: str = "c"
+
+    def __init__(self):
+        pass
+
+c = C()
+print(c.__dict__)
+print(c.attr)
+
+@dataclass
+class D:
+    attr: int = 1
+    percent: str = "ok"
+
+    def __init__(self):
+        pass
+
+"""
+类变量在外部，可以通过intance.类变量 来访问，但是在其重新赋值后，两者就分开了
+"""
+print("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
+d = D()
+print(d.__dict__)
+print(d.attr)
+print(id(d.attr))
+print(D.attr)
+print(id(D.attr))
+d.attr = 10
+print(d.attr)
+print(D.attr)
+print(d.percent)
+print("instance percent :{}, class percent:{}".format(id(d.percent),id(D.percent)))
+d.percent = "failed"
+print("instance percent :{}, class percent:{}".format(id(d.percent),id(D.percent)))
+print(d.percent)
+print(D.percent)
