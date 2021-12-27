f5dd5031932a09840b8040d18e75a6abd6794a38
lipeizhao <lipeizhao@encootech.com>
2021-12-27 17:11:40 +0800

    update

--- base/descriptor_deep.py
+++ base/descriptor_deep.py
@@ -3,7 +3,8 @@
 
 当找一个属性时，先check该属性是否是类属性是否是一个descriptor, 如果是，则调用其__get__()方法
 
-当一个instance属性和描述符重名时候，先调用instance属性，如果失败，再查找描述符
+当一个instance属性和non-data描述符重名时候，先调用instance属性，如果失败，再查找描述符
+当一个instance属性和data描述符重名时候，先调用data描述符
 """
 
 class NonData_Destriptor(object):
@@ -13,6 +14,17 @@ class NonData_Destriptor(object):
     def __get__(self, instance, owner):
         print("call in NonData_Destriptor __get__")
         return self.name
+
+class Data_Destriptor(object):
+    def __init__(self, default = "student"):
+        self.name = default
+
+    def __get__(self, instance, owner):
+        print("call in Data_Destriptor __get__")
+        return self.name
+
+    def __set__(self, instance, value):
+        pass
 
 class Student(object):
     name = NonData_Destriptor()
@@ -25,8 +37,21 @@ class Student(object):
         print("call in Student.__getattribute__")
         return super().__getattribute__(item)
 
-print("instance 属性和 descriptor 重名")
+class Student_2(object):
+    name = Data_Destriptor()
+
+    def __init__(self, name):
+        self.name = name
+
+    def __getattribute__(self, item):
+        print("call in Student.__getattribute__")
+        return super().__getattribute__(item)
+
+print("instance 属性和 Non_data descriptor 重名")
 s1 = Student("Miki")
 print(s1.name)
 print("描述符如何被调用")
 print(s1.test)
+print("instance 属性和 data descriptor 重名")
+s2 = Student_2("Miki")
+print(s2.name)
