03fdbdf5627efb7e39dde14998f6d15e4ecdb937
lipeizhao <lipeizhao@encootech.com>
2022-01-12 14:29:54 +0800

    add iterable

---
+++ base/iterable.py
@@ -0,0 +1,45 @@
+"""
+1. An object can be iterated over with "for" if it implements
+   __iter__() or __getitem__().
+
+2. An object can function as an iterator if it implements next().
+"""
+
+class Foo_getitem(object):
+    def __init__(self, count):
+        self.count = count
+
+    def __getitem__(self, index):
+        print("call in __getitem__ {}".format(index))
+        if index > self.count:
+            raise IndexError()
+        return index
+
+
+class Foo_iter(object):
+    def __init__(self, count):
+        self.count = count
+
+    def __iter__(self):
+        print("call in __iter__" )
+        return self
+
+    def __next__(self):
+        print("call in __next__ {}".format(self.count))
+        self.count -= 1
+        if self.count == 0:
+            raise StopIteration()
+
+
+print("The __getitem__ implement in the for statement")
+foo_getitem = Foo_getitem(5)
+for i in foo_getitem:
+    print(i)
+
+print("*****************************")
+print("The __iter__  and __next__ implement in the for statement")
+
+foo_iter = Foo_iter(5)
+for i in foo_iter:
+    print(i)
+
